39323ffc5b057a6288430db3a83e3d06f84065dc
Alexander Campbell <acampbell2231@gmail.com>
2017-08-25 09:33:06 -0600

    MemoryMatch

---
+++ main.js
@@ -0,0 +1,55 @@
+$(document).ready( function(){
+
+
+//add common class to all cards
+//listen for when any car is clicked
+//reveal the child of clicked cards
+  $("#one").click(function(){
+    $("#pandaone").show();
+  });
+
+  $("#two").click(function(){
+    $("#pandatwo").show();
+  });
+
+  $("#three").click(function(){
+    $("#pandathree").show();
+  });
+
+  $("#four").click(function(){
+    $("#pandafour").show();
+  });
+
+  $("#five").click(function(){
+    $("#foxone").show();
+  });
+
+  $("#six").click(function(){
+    $("#foxtwo").show();
+  });
+
+  $("#seven").click(function(){
+    $("#foxthree").show();
+  });
+
+  $("#eight").click(function(){
+    $("#foxfour").show();
+  });
+
+  $("#nine").click(function(){
+    $("#wolfone").show();
+  });
+
+  $("#ten").click(function(){
+    $("#wolftwo").show();
+  });
+
+  $("#eleven").click(function(){
+    $("#wolfthree").show();
+  });
+  $("#twelve").click(function(){
+    $("#wolffour").show();
+  });
+
+
+})
